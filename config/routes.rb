e6ca653f5ac679bcd4a9a993f4f9a8a4db9b40a3
sriman <sriman6907@gmail.com>
2022-11-30 17:06:21 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  #devise_for :users
+  namespace :v1 do
+    resources :contacts
+    resources :sessions, only: [:create, :destroy]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+
